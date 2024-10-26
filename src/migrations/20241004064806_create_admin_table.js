4612d08f0e89c20364c8db5b459e4ef0a6e2b3df
Subarna Poudel <subarnapdl@gmail.com>
2024-10-26 14:35:01 +0545

    refactor: clean fields

--- src/migrations/20241004064806_create_admin_table.js
+++ src/migrations/20241004064806_create_admin_table.js
@@ -7,12 +7,12 @@ exports.up = function (knex) {
       table.bigIncrements('id').primary();
       table.string('name').notNullable();
       table.string('phone_number').notNullable();
+      table.string('email').notNullable().unique();
       table.string('company').notNullable();
       table.string('address').notNullable();
-      table.string('email').notNullable().unique();
       table.string('profile_image');
       table.string('password').notNullable();
-      table.enum('role', ['0', '1', '2', '3']).notNullable().defaultTo('1'); //0 is superAdmin and 1 is admin 
+      table.enum('role', ['0', '1']).notNullable().defaultTo('1'); //0 is superAdmin and 1 is admin 
       table.enum('status', ['0', '1']).notNullable().defaultTo('0');
       table.timestamp('created_at').defaultTo(knex.fn.now());
     });
--- src/migrations/20241007122524_create_route_table.js
+++ src/migrations/20241007122524_create_route_table.js
@@ -6,12 +6,9 @@ exports.up = function (knex) {
   return knex.schema.createTable('routes', function (table) {
     table.increments('id').primary(); // Primary key for the Route
     table.bigInteger('admin_id').unsigned().references('id').inTable('admins').onDelete('CASCADE');
-    table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE');
     table.string('name').notNullable(); // Route name
+    table.date('date').notNullable(); // Date of the route
     table.string('note'); // Additional notes about the route
-    table.enu('optimize_mode', ['shortest', 'fastest']).notNullable(); // Optimization mode
-    table.enu('travel_mode', ['driving', 'bicycling', 'walking']).notNullable(); // Travel mode
-    table.boolean('is_optimized').defaultTo(false); // Indicates if the route is optimized
     table.boolean('is_deleted').defaultTo(false);
     table.timestamps(true, true);
   });
