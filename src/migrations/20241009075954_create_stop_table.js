093c1b22c56f90a6ac4fce46874ddbdcc22346e9
Bijaya Poudel <poudelbijaya25@gmail.com>
2024-10-26 14:22:36 +0545

    user id added in stop

--- src/migrations/20241009075954_create_stop_table.js
+++ src/migrations/20241009075954_create_stop_table.js
@@ -2,23 +2,24 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up=function(knex){
-    return knex.schema.createTable('stops', function(table){
-        table.increments('id').primary();
-        table.integer('route_id').unsigned().references('id').inTable('routes').onDelete('CASCADE');
-        table.integer('order').notNullable();
-        table.string('name').notNullable();
-        table.decimal('latitude').notNullable();
-        table.decimal('longitude').notNullable();
-        table.boolean('is_deleted').defaultTo(false);
-        table.timestamps(true, true);
-    });
-}
+exports.up = function (knex) {
+  return knex.schema.createTable('stops', function (table) {
+    table.increments('id').primary();
+    table.integer('route_id').unsigned().references('id').inTable('routes').onDelete('CASCADE');
+    table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE');
+    table.integer('order').notNullable();
+    table.string('name').notNullable();
+    table.decimal('latitude').notNullable();
+    table.decimal('longitude').notNullable();
+    table.boolean('is_deleted').defaultTo(false);
+    table.timestamps(true, true);
+  });
+};
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
+exports.down = function (knex) {
   return knex.schema.dropTable('stops');
 };
