b40a12ec0ebbd8efdddeb3abfb28854f68292535
Subarna Poudel <subarnapdl@gmail.com>
2024-10-29 09:17:03 +0545

    refactor: remove delivery status table & use stop table for it

--- src/migrations/20241009075954_create_stop_table.js
+++ src/migrations/20241009075954_create_stop_table.js
@@ -11,6 +11,11 @@ exports.up = function (knex) {
     table.string('name').notNullable();
     table.decimal('latitude').notNullable();
     table.decimal('longitude').notNullable();
+    // 0 is pending, 1 is in progress, 2 is completed, 3 is cancelled, 4 is postponed
+    table.enum('delivery_status', ['0', '1', '2', '3', '4']).notNullable().defaultTo('0');
+    table.string('note');
+    // 0 to 5
+    table.integer('ratings').notNullable();
     table.boolean('is_deleted').defaultTo(false);
     table.timestamps(true, true);
   });
